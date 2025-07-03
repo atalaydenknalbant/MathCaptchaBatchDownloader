dfcbf8622223ff5505b0c69bae0e3a2a911c06e7
Atalay <atalaydenknalbant@hotmail.com>
2025-07-03 21:39:24 +0300

    Update Files

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -7,16 +7,22 @@ namespace MathCaptchaBatch
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            // Ignore WebResource.axd etc
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("BulkCaptchas", "Home/BulkCaptchas",
-                new { controller = "Home", action = "BulkCaptchas" });
+            // 1️⃣ Empty URL: go to Home/BulkCaptchas
+            routes.MapRoute(
+                name: "Root",
+                url: "",
+                defaults: new { controller = "Home", action = "BulkCaptchas" }
+            );
 
-            routes.MapRoute("BatchPartial", "Home/BatchPartial",
-                new { controller = "Home", action = "BatchPartial" });
-
-            routes.MapRoute("Default", "{controller}/{action}/{id}",
-                new { controller = "Home", action = "BulkCaptchas", id = UrlParameter.Optional });
+            // 2️⃣ Default MVC route for everything else
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "BulkCaptchas", id = UrlParameter.Optional }
+            );
         }
     }
 }
